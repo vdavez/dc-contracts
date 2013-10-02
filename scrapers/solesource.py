afb337c4e92fd9dd0059ab7c61de0a95cfb52224
vzvenyach <vzvenyach@dccouncil.us>
2013-10-02 07:26:02 -0400

    early updates

--- scrapers/solesource.py
+++ scrapers/solesource.py
@@ -1,5 +1,13 @@
 #Sole Source Scraper
-from pyquery import PyQuery
+from pyquery import PyQuery as pq
 
-d = PyQuery(url="http://app.ocp.dc.gov/intent_award/intent_award.asp")
+d = pq(url="http://app.ocp.dc.gov/intent_award/intent_award.asp")
 
+# print "This is the title of the scraped website:"
+# print d("title").text()
+
+## Here's how this is going to work
+# The content is in a table in the div class "contentContainer".
+# The table has a first row with headers
+# The second row and all subsequent rows are the contents.
+# Within the fourth column, there is a link. That's the D&F PDF.
